d9b90ee273d1c5f457df6de3fe80ac510c20ee8b
unknown <victoriasugden@yahoo.co.uk>
2024-01-27 13:48:01 +0000

    added some functions

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,93 @@
-//My openWeather API key.
+//My openWeather link and API key.
+var geoURL = "https://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid={API key}"
 var APIKey = "e8e64517935ca0b0e88c99dd00d7fc00";
+//5 day forecast link (add https)
+var weatherURL = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}"
 
-var city;
+//var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+
+// DOM element references (get all id's)
+var searchForm = document.getElementById('#search-form');
+var searchInput = document.getElementById('#search-input');
+var searchBtn = document.getElementById('#search-button');
+var todayContainer = document.getElementById('#today');
+var forecastContainer = document.getElementById('#forecast');
+var historyContainer = document.getElementById('#history');
+var serachForm
+
+//create event listener and call out function name (for search button)
+searchBtn.addEventListener("click", searchCity)
+
+//validate the input of the city, then call out next function
+function searchCity() {
+
+    e.preventDefault()
+    var citty = searchInput.ariaValueMax.trim()
+    geoCoords(city)
+
+    searchInput.value.innerHTML = ""
+}
+//fetch to lat and lon from the geoURL and the call out to functions
+function geoCoords(city) {
+    console.log(city);
+
+    var geoURL = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${apiKey}`
+}
+//fetching the city and its coorinates
+fetch(geoURL)
+    .then(function (response) {
+        return response.json();
+    }).then(function (data) {
+        console.log(data[0]);
+        weatherCoord(data[0],city)
+    }
+    )
+
+    //purpose of this function is to use geo fetch data and get weather ready to call out functions to display current day and forecast
+    function weatherCoord(location, city){//changed "data[0]" to location as code didnt like the [0]
+        console.log(location, city);
+        var {lon, lat} = location
+        var city = location.name
+
+        var weatherURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`
+    }
+
+    //fething the weather for current and forecast (copy & paste fetch)
+    fetch(weatherURL)
+    .then(function (response) {
+        return response.json();
+    }).then(function (data) {
+        console.log(data[0]);
+       //data for current day weather 1 day
+        currentDay(data.list[0], city)
+       //data for 5 day forecast
+       forcast5Day(data.list)
+    }
+    )
+
+    function currentDay(current, city){
+        //current = data[0]
+        console.log(current);
+        //pull data info from api
+        var temp = current.main.temp
+        console.log(temp);
+    }
+//create card to list information
+
+var card = document.createElement('div')
+var cardBody = document.createElement('div')
+
+card.append(cardBody)
+//cardBody elements - title - text
+var cardTitle
+var cardTextTemp =
+
+//join the card and date
+cardTitle.textcontent = city
+
 
 //The OpenWeather Current Weather Data documentation provides an example of how to make an API call using just the city name, as shown in the following code:
 
-api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
 
 //string concatenation to create a new variable called queryURL, which will store the OpenWeather Current Weather Data URL and the necessary variables.
 /*http://api.openweathermap.org/data/2.5/weather is the base URL for calling the Current Weather Data API.
@@ -21,8 +103,7 @@ Next, we concatenate the other required parameter, appid=, where we'll add the A
 
 Finally, we concatenate the APIKey variable that contains the key we obtained at the beginning of this guide.  */
 
-var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+
 
 //Now that i have created A query URL, i only need to call the Fetch API to pass the query URL in as a parameter:
 
-fetch(queryURL)
